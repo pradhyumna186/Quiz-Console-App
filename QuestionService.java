ae53e6de90f1d18fc0d90f19f31c44337fe4fde9
pradhyumna186 <pradhyumna186@gmail.com>
2025-05-19 19:06:37 -0400

    added a play Quiz method and implemented score

--- QuestionService.java
+++ QuestionService.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class QuestionService {
 
     Question [] questions = new Question[5];
+    String selection[] = new String[5];
     public QuestionService()
     {
         questions[0] = new Question(1, "Which language is used for Android app development?", "JAVA", "CPP", "PYTHON", "C#", "JAVA");
@@ -11,10 +14,42 @@ public class QuestionService {
     }
  
 
-    public void displayQuestions()
-    {
+    public void playQuiz()
+    {   
+        int i =0;
         for(Question q : questions)
-        System.out.println(q);
+        {
+        System.out.println("Question no. : "+q.getId());
+        System.out.println(q.getQuestion());
+        System.out.println(q.getOpt1());
+        System.out.println(q.getOpt2());
+        System.out.println(q.getOpt3());
+        System.out.println(q.getOpt4());
+        Scanner sc = new Scanner(System.in);
+        selection[i] = sc.nextLine();
+        i++;
+
+        }
+        for(String s : selection)
+        {
+            System.out.println(s);
+        }
+    }
+
+    public void printScore()
+    {
+        int score = 0;
+        for(int i = 0;i<questions.length;i++)
+        {
+            Question que = questions[i];
+            String actualAnswer = que.getAnswer();
+
+            String userAnswer = selection[i];
+            if (actualAnswer.equals(userAnswer)){
+                score++;
+            }
+        }
+        System.out.println("Your score is : "+score);
     }
 
 
